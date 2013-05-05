ba6c0bfec367856f5a5eb36f557dbae5b7755184
Anatoly Mironov <mirontoli@gmail.com>
2013-05-06 00:21:05 +0200

    Remember the scroll position of the listview

--- WindowsStore/ChuvashDict/pages/home/home.js
+++ WindowsStore/ChuvashDict/pages/home/home.js
@@ -1,6 +1,7 @@
 ﻿(function () {
     "use strict";
-
+    
+    var app = WinJS.Application;
     var appView = Windows.UI.ViewManagement.ApplicationView;
     var appViewState = Windows.UI.ViewManagement.ApplicationViewState;
     var nav = WinJS.Navigation;
@@ -8,43 +9,54 @@
 
     //global words var
     var words = ["hej", "hå"];
+    var wordCloud;
+    var bound = false;
 
     ui.Pages.define("/pages/home/home.html", {
 
         ready: function (element, options) {
-
-
+            wordCloud = document.getElementById("wordCloud").winControl;
+            wordCloud.onloadingstatechanged = function() {
+                if (app.sessionState.homeScrollPosition && wordCloud.loadingState == "viewPortLoaded") {
+                    wordCloud.scrollPosition = app.sessionState.homeScrollPosition;
+                    app.sessionState.homeScrollPosition = null;
+                }
+            };
             bindWordCloud();
+
 
         },
 
         updateLayout: function () {
 
+        },
+        unload: function () {
+            //http://www.codefoster.com/post/2013/01/08/loadingstates
+            //Remember the scroll position of the listview
+            app.sessionState.homeScrollPosition = wordCloud.scrollPosition;
         }
 
     });
     // This function is run once on page load
     function bindWordCloud() {
         // Add WinJS Control For Listview
-        var wordCloud = document.getElementById("wordCloud").winControl;
         wordCloud.template = document.getElementById("wordTemplate");
         wordCloud.loadingBehavior = "incremental";
         wordCloud.selectionMode = "single";
 
 
         // Bind datas
-        WinJS.xhr({ url: "words/words-part001.csv" }).then(function (request) {
+        WinJS.xhr({ url: "words/words.csv" }).then(function (request) {
             words = request.responseText.split("\r\n");
             var listItems = new WinJS.Binding.List(words);;
             wordCloud.itemDataSource = listItems.dataSource;
             wordCloud.oniteminvoked = function (e) {
                 //http://code.msdn.microsoft.com/windowsapps/Navigation-sample-cf242faa/sourcecode?fileId=44084&pathId=1713588077
                 e.detail.itemPromise.then(function (item) {
-                    //tmp(item.data);
                     return WinJS.Navigation.navigate("/pages/detail/detail.html", item.data);
                 });
             };
-
+            bound = true;
         });
 
 
